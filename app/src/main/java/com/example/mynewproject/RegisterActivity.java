cb50ffbfcce1ce44b082319ff293f265f5aab2ac
Dell <shivamsarthak24@gmail.com>
2019-05-03 14:57:43 +0530

    My First Project

---
+++ app/src/main/java/com/example/mynewproject/RegisterActivity.java
@@ -0,0 +1,37 @@
+package com.example.mynewproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+    EditText mTextUsername;
+    EditText mTextPassword;
+    EditText mTextCnfPassword;
+    Button mButtonRegister;
+    TextView mTextViewLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mTextUsername = (EditText)findViewById(R.id.edittext_username);
+        mTextPassword = (EditText)findViewById(R.id.edittext_password);
+        mTextCnfPassword = (EditText)findViewById(R.id.edittext_cnf_password);
+        mButtonRegister = (Button)findViewById(R.id.button_login);
+        mTextViewLogin = (TextView)findViewById(R.id.textview_register);
+        mTextViewLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+
+            }
+        });
+    }
+}
